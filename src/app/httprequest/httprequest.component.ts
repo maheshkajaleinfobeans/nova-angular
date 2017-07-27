41d620cb7ac65c778ac7cc979114cb8916948f22
Mahesh Kajale <mahesh.kajale@infobeans.com>
2017-07-27 17:49:08 +0530

    charcter count decreased

--- src/app/httprequest/httprequest.component.ts
+++ src/app/httprequest/httprequest.component.ts
@@ -11,51 +11,51 @@ import { Observable } from 'rxjs/Observable';
 })
 @Injectable()
 export class HttprequestComponent implements OnInit{
-  
+
   private single;
   private top;
   private featured;
   private token;
-  
+  private apiUrl = 'http://127.0.0.1:8001/';
   constructor(private http: Http) {}
 
   ngOnInit() {
-  this.getToken();
+    this.getToken();
   }
 
   
   getTopAlbum(token) {
     const headers = new Headers({'Authorization': 'Bearer ' +token});
-    return this.http.get('http://127.0.0.1:8000/top-albums/us',{headers: headers})
-  .toPromise()
-  .then(response => {
-    this.top = response.json();
+    return this.http.get(this.apiUrl+'top-albums/us',{headers: headers})
+    .toPromise()
+    .then(response => {
+      this.top = response.json();
     }
     )
-  .catch(this.handleError);
+    .catch(this.handleError);
   }
   
   getSingleAlbum(token) {
     const headers = new Headers({'Authorization': 'Bearer ' +token});
-    return this.http.get('http://127.0.0.1:8000/top-singles/us',{headers: headers})
-  .toPromise()
-  .then(response => {
-    this.single = response.json();
+    return this.http.get(this.apiUrl+'top-singles/us',{headers: headers})
+    .toPromise()
+    .then(response => {
+      this.single = response.json();
     }
     )
-  .catch(this.handleError);
+    .catch(this.handleError);
   }
   
   
   getFeaturedAlbum(token) {
     const headers = new Headers({'Authorization': 'Bearer ' +token});
-    return this.http.get('http://127.0.0.1:8000/featured-artist/us',{headers: headers})
-  .toPromise()
-  .then(response => {
-    this.featured = response.json();
+    return this.http.get(this.apiUrl+'featured-artist/us',{headers: headers})
+    .toPromise()
+    .then(response => {
+      this.featured = response.json();
     }
     )
-  .catch(this.handleError);
+    .catch(this.handleError);
   }
 
   private handleError(error: any): Promise<any> {
@@ -64,8 +64,8 @@ export class HttprequestComponent implements OnInit{
   }
 
   getToken() {
-    return this.http.post('http://127.0.0.1:8000/oauth/v2/token',
-      {
+    return this.http.post(this.apiUrl+'oauth/v2/token',
+    {
       grant_type: 'password',
       client_id: '1_3bcbxd9e24g0gk4swg0kwgcwg4o8k8g4g888kwc44gcc0gwwk4',
       client_secret: '4ok2x70rlfokc8g0wws8c8kwcokw80k44sg48goc0ok4w0so0k',
@@ -73,15 +73,15 @@ export class HttprequestComponent implements OnInit{
       password: 'admin'
     },
     )
-   .toPromise()
+    .toPromise()
     .then(response => {
-    this.token = response.json();
-    this.getTopAlbum(this.token.access_token);
-    this.getSingleAlbum(this.token.access_token);
-    this.getFeaturedAlbum(this.token.access_token);
+      this.token = response.json();
+      this.getTopAlbum(this.token.access_token);
+      this.getSingleAlbum(this.token.access_token);
+      // this.getFeaturedAlbum(this.token.access_token);
     }
     )
-  .catch(this.handleError);
+    .catch(this.handleError);
   }
 
 }
